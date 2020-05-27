dc2bc63efa52dad1591e2c63205af14626c96841
Carlos Henrique <chsjr1996@gmail.com>
2020-05-27 00:05:01 -0300

    Fix error in spawn electron dev mode adding no sandbox arg

--- scripts/start-dev.js
+++ scripts/start-dev.js
@@ -5,8 +5,4 @@ let spawn = require("child_process").spawn;
 
 const main = join(__dirname, "../dist/browser/main.js");
 
-spawn(electron, [main], {
-  command: electron,
-  args: [main, "no-sandbox"],
-  stdio: "inherit"
-});
+spawn(electron, [main, "--no-sandbox"]);
